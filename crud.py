9a469e2560c06476745038d571c4729a646b1b3c
MartinKraychev <83907783+MartinKraychev@users.noreply.github.com>
2023-11-12 22:41:04 +0000

    Add plan

--- crud.py
+++ crud.py
@@ -4,7 +4,7 @@ import models
 import schemas
 
 
-def get_user(db: Session, user_id: int):
+def get_user_by_id(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
@@ -16,10 +16,6 @@ def get_user_by_phone_number(db: Session, phone_number: str):
     return db.query(models.User).filter(models.User.phone_number == phone_number).first()
 
 
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(email=user.email, hashed_password=fake_hashed_password, phone_number=user.phone_number)
@@ -28,6 +24,10 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.refresh(db_user)
 
     return db_user
+
+
+def get_property(db: Session, property_id: int):
+    return db.query(models.Property).filter(models.Property.id == property_id).first()
 
 
 def get_properties(db: Session):
--- main.py
+++ main.py
@@ -35,8 +35,8 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -51,3 +51,22 @@ def create_property(prop: schemas.PropertyCreate, db: Session = Depends(get_db))
 def get_properties(db: Session = Depends(get_db)):
     properties = crud.get_properties(db)
     return properties
+
+
+@app.get("/properties/{property_id}", response_model=schemas.Property)
+def get_property_by_id(property_id: int, db: Session = Depends(get_db)):
+    prop = crud.get_property(db, property_id)
+    if prop is None:
+        raise HTTPException(status_code=404, detail="Property not found")
+    return prop
+
+
+# Todo
+# Login ,register, logout using token
+# CRUD property
+# My properties, user == user.id
+# Add rating, check if user has rated. Users cannot rate own properties, all ratings
+# Meet the team - users with most properties
+# permissions, some views are for everybody and some are only for logged in users
+# Unauthorized page, 404?
+
