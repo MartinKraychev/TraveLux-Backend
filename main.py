bad01968699aba7c6f1ed830d001af57398ed19f
MartinKraychev <83907783+MartinKraychev@users.noreply.github.com>
2023-11-21 20:32:14 +0000

    Addec cors

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ from views.auth import router as auth_router
 from views.property import router as property_router
 from views.rating import router as rating_router
 from db_info import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Configure CORS
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(property_router, prefix="/properties", tags=["properties"])
