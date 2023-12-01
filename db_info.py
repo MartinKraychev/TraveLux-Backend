656ae4ccc213aaa65d41fa29ee72c9aa74c1b6ad
MartinKraychev <83907783+MartinKraychev@users.noreply.github.com>
2023-12-01 18:06:16 +0000

    Add remote postgre instance

--- db_info.py
+++ db_info.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost/travelux"
+SQLALCHEMY_DATABASE_URL = "postgresql://my_user:4295aHUJXgQzkBbasijrt7ogw8pmku6x@dpg-cll15g4jtl8s73f580a0-a.frankfurt-postgres.render.com/travelux_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
